cfce9a306082fe6740a7fa65b502ddf22b86458b
Shahar Kofman <shaharkofman@mail.tau.ac.il>
2025-09-13 09:24:26 +0300

    Refactor C API method declarations for conditional inclusion
    Moved C API method declarations inside an ifdef block to ensure they are only exposed when Python.h is included.

--- symnmf.h
+++ symnmf.h
@@ -1,11 +1,14 @@
-#include <Python.h>
-
 double** calculate_sym_matrix(double **data_points, int N, int d);
 double** calculate_ddg_matrix(double **data_points, int N, int d);
 double** calculate_norm_matrix(double **data_points, int N, int d);
 double** optimize_h(double** W, double** init_H, int N, int k);
 void free_matrix(double **matrix, int N);
-static PyObject* sym_capi(PyObject *self, PyObject *args);
-static PyObject* ddg_capi(PyObject *self, PyObject *args);
-static PyObject* norm_capi(PyObject *self, PyObject *args);
-static PyObject* symnmf_capi(PyObject *self, PyObject *args);
+
+/* C API methods, ifdef block in order to expose only if <Python.h> is included */
+#ifdef PY_SSIZE_T_CLEAN
+#include <Python.h>
+PyObject* sym_capi(PyObject *self, PyObject *args);
+PyObject* ddg_capi(PyObject *self, PyObject *args);
+PyObject* norm_capi(PyObject *self, PyObject *args);
+PyObject* symnmf_capi(PyObject *self, PyObject *args);
+#endif
